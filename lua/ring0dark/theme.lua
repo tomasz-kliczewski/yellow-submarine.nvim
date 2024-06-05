020221257536727a2cdf6eca38648820799f0a13
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-06-05 14:36:08 +0300

    change colors

--- lua/ring0dark/theme.lua
+++ lua/ring0dark/theme.lua
@@ -1,34 +1,27 @@
-
-local c = require('ring0dark.palette')
+local c = require("ring0dark.palette")
 
 local hl = vim.api.nvim_set_hl
 local theme = {}
 
 theme.set_highlights = function()
-
-  -- highlights
-  hl(0, "Normal", { fg = c.fg, bg = c.bg })
-  hl(0, "SignColumn", { fg = 'NONE', bg = c.bg })
-  hl(0, "MsgArea", { fg = c.fg, bg = c.bg })
-  hl(0, "ModeMsg", { fg = c.fg, bg = c.bg })
-  hl(0, "MsgSeparator", { fg = c.fg, bg = c.bg })
-  hl(0, "SpellBad", { fg = c.red, bg = 'NONE', underline=true, })
-  hl(0, "SpellCap", { fg = c.yellow, bg = 'NONE', underline=true, })
-  hl(0, "SpellLocal", { fg = c.green, bg = 'NONE', underline=true, })
-  hl(0, "SpellRare", { fg = c.purple, bg = 'NONE', underline=true, })
-  hl(0, "NormalNC", { fg = c.fg, bg = c.bg })
-  hl(0, "Pmenu", { fg = c.red, bg = c.bg, sp = 'NONE', blend=50,  })
-  hl(0, "PmenuSel", { fg = 'NONE', bg = c.blue })
-  hl(0, "WildMenu", { fg = c.fg, bg = c.blue })
-  hl(0, "CursorLineNr", { fg = c.yellow, bg = 'NONE', bold=true, })
-  hl(0, "Comment", { fg = c.brown, bg = 'NONE', italic=true, })
-
-  -- Treesitter
-  hl(0, "TSComment", { link = 'Comment' })
-  hl(0, "TSAnnotation", { fg = c.blue, bg = 'NONE' })
-  hl(0, "TSAttribute", { fg = c.yellow, bg = 'NONE' })
-  hl(0, "TSConstructor", { fg = c.yellow, bg = 'NONE' })
-  hl(0, "TSType", { fg = c.yellow, bg = 'NONE' })
+	-- highlights
+	hl(0, "Normal", { fg = c.fg, bg = c.bg })
+	hl(0, "SignColumn", { fg = "NONE", bg = c.bg })
+	hl(0, "MsgArea", { fg = c.fg, bg = c.bg })
+	hl(0, "ModeMsg", { fg = c.fg, bg = c.bg })
+	hl(0, "MsgSeparator", { fg = c.fg, bg = c.bg })
+	hl(0, "SpellBad", { fg = c.red, bg = "NONE", underline = true })
+	hl(0, "SpellCap", { fg = c.yellow, bg = "NONE", underline = true })
+	hl(0, "SpellLocal", { fg = c.green, bg = "NONE", underline = true })
+	hl(0, "SpellRare", { fg = c.purple, bg = "NONE", underline = true })
+	hl(0, "NormalNC", { fg = c.fg, bg = c.bg })
+	hl(0, "Pmenu", { fg = c.red, bg = c.bg, sp = "NONE", blend = 50 })
+	hl(0, "PmenuSel", { fg = "NONE", bg = c.blue })
+	hl(0, "WildMenu", { fg = c.fg, bg = c.blue })
+	hl(0, "CursorLineNr", { fg = c.yellow, bg = "NONE", bold = true })
+	hl(0, "Comment", { fg = c.brown, bg = "NONE", italic = true })
+	hl(0, "Identifier", { fg = c.red, bg = "NONE", italic = true })
 end
 
 return theme
+
