e5334061ef66e76eb1fb13d1be50a1c07a92a383
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-07-06 10:22:48 +0300

    Update palette.lua

--- lua/ring0dark/palette.lua
+++ lua/ring0dark/palette.lua
@@ -1,7 +1,7 @@
 local colors = {
 	fg = "#cdd6f4",
-	bg = "#181818",
-	bg_darker = "#101010",
+	bg = "#191724",
+	bg_lighter = "#26233a",
 	white = "#cdd6f4",
 	gray = "#282828",
 	green = "#73d936",
@@ -13,6 +13,7 @@ local colors = {
 	orange = "#c18a56",
 	purple = "#b668cd",
 	magenta = "#D16D9E",
+	string_lit = "#efbc74",
 }
 
 return colors
