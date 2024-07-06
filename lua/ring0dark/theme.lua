67e9086dc70cc83d9618ec86ae595166b9e2bca7
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-07-06 10:23:55 +0300

    Update theme.lua

--- lua/ring0dark/theme.lua
+++ lua/ring0dark/theme.lua
@@ -6,16 +6,16 @@ local theme = {}
 theme.set_highlights = function()
 	-- highlights
 	hl(0, "Normal", { fg = c.fg, bg = c.bg })
-	hl(0, "NormalFloat", { fg = c.fg, bg = c.bg_darker })
+	hl(0, "NormalFloat", { fg = c.fg, bg = c.bg_lighter })
 	hl(0, "FloatBorder", { fg = c.fg, bg = c.bg })
 	hl(0, "NormalNC", { fg = c.fg, bg = c.bg }) -- Normal non current
-	hl(0, "NormalSB", { fg = c.fg, bg = c.bg_darker }) -- Normal text in side bar
-	hl(0, "Pmenu", { fg = c.fg, bg = c.bg_darker })
+	hl(0, "NormalSB", { fg = c.fg, bg = c.bg_lighter }) -- Normal text in side bar
+	hl(0, "Pmenu", { fg = c.fg, bg = c.bg_lighter })
 	hl(0, "SignColumn", { fg = "NONE", bg = c.bg })
 	hl(0, "CursorLineNr", { fg = c.yellow, bg = "NONE", bold = true })
 	hl(0, "Statement", { fg = c.yellow, bg = "NONE", bold = true })
 	hl(0, "TelescopeSelection", { fg = c.fg, bg = c.gray, italic = true })
-	hl(0, "CursorLine", { fg = "NONE", bg = c.gray, italic = true })
+	hl(0, "CursorLine", { fg = "NONE", bg = bg_lighter, italic = true })
 	hl(0, "StatusLine", { fg = c.fg, bg = c.gray, italic = true })
 
 	-- special words
@@ -24,7 +24,7 @@ theme.set_highlights = function()
 	hl(0, "@variable", { fg = c.fg, bg = "NONE", italic = true })
 	hl(0, "Constant", { fg = c.fg, bg = "NONE", italic = true })
 	hl(0, "Function", { fg = c.fg, bg = "NONE", italic = true })
-	hl(0, "String", { fg = c.green, bg = "NONE", italic = true })
+	hl(0, "String", { fg = c.string_lit, bg = "NONE", italic = true })
 	hl(0, "Identifier", { fg = c.fg, bg = "NONE", italic = true })
 	hl(0, "Title", { fg = c.yellow, bg = "NONE", bold = true })
 	hl(0, "Type", { fg = c.fg, bg = "NONE", bold = true })
